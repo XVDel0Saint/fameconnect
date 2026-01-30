10a95b66fc95b085c7f19ef92a195bdaf5a29832
Xyrel Vaughn Delos Santos <delossantosxyrelvaughn@gmail.com>
2026-01-30 19:57:33 +0800

    removed frontend hyphen name

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['https://fameconnect-frontend.onrender.com', 'http://localhost:3000'],
+    'allowed_origins' => ['https://fameconnect.onrender.com', 'http://localhost:3000'],
 
     'allowed_origins_patterns' => [],
 
