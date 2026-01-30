5c196e8479da1d9e64cc2888bcbf02b7b71b7e74
Xyrel Vaughn Delos Santos <delossantosxyrelvaughn@gmail.com>
2026-01-30 21:53:13 +0800

    for production build

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -2,6 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 
+use Illuminate\Support\Facades\Artisan;
+
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/force-clear-cache', function () {
+    Artisan::call('config:clear');
+    Artisan::call('route:clear');
+    return "Cache is cleared! You can delete this route now.";
 });
