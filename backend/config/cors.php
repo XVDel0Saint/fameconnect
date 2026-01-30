a5fcf6bd4d316d491918cc0ebeb48665490577ec
Xyrel Vaughn Delos Santos <delossantosxyrelvaughn@gmail.com>
2026-01-30 20:11:18 +0800

    missing proper CORS management

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -19,7 +19,10 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => [
+        'https://fameconnect.onrender.com',
+        'http://localhost:3000',
+    ],
 
     'allowed_origins_patterns' => [],
 
